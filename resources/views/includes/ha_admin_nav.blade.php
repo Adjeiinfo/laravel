6a386fadccb37ee01743edad18c863b77d159829
Oracle Adjeiinfo <koffieli@gmail.com>
2018-09-15 22:43:13 +0900

    on image

--- resources/views/includes/ha_admin_nav.blade.php
+++ resources/views/includes/ha_admin_nav.blade.php
@@ -1,10 +1,8 @@
    <!-- Navigation -->
-    <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-
-        <!--Admin Top navigation-->
-        @include('includes.ha_admin_top_nav')
-        
-        <!--Admin side navigation -->
-        @include('includes.ha_side_bar_nav')
-        <!-- /.navbar-static-side -->
-    </nav>
+   <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+   	<!--Admin Top navigation-->
+   	@include('includes.ha_admin_top_nav')
+   	<!--Admin side navigation -->
+   	@include('includes.ha_side_bar_nav')
+   	<!-- /.navbar-static-side -->
+ </nav>
